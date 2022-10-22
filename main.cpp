57364cf0f740568c8cb38fff9cbcaec9bb8599e7
Oleksii Vodka <oleksii.vodka@gmail.com>
2022-10-22 17:17:20 +0300

    Lesson2: add new test case

--- main.cpp
+++ main.cpp
@@ -13,5 +13,6 @@ int main()
     cout << "Hello world" << endl;
     cout << "Hello from linux" << endl;
     cout << "2 + 3 = " << add(2, 3) << endl;
+    cout << "3 + 8 = " << add(3, 8) << endl;
     return 0;
 }
